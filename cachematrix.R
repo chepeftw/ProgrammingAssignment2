46ac115195b0d98c826ee42b5b61e47dd12dd8bd
Jose Alvarez <jalvarez@xumak.com>
2015-01-24 21:10:46 -0600

    Enhancing functionality to the makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -4,22 +4,31 @@
 ## Write a short comment describing this function
 ## makeCacheMatrix stores the matrix and the mean, also describes the setters and getters.
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
+  inv <- NULL
   set <- function(y) {
     x <<- y
-    m <<- NULL
+    inv <<- NULL
   }
   get <- function() x
-  setmean <- function(mean) m <<- mean
-  getmean <- function() m
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
   list(set = set, get = get,
-       setmean = setmean,
-       getmean = getmean)
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
         ## Return a matrix that is the inverse of 'x'
 }
